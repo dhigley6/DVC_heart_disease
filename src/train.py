9457205ac633fb6ba068e81582e47da1f96c8bb7
dhigley6 <dhigley@stanford.edu>
2021-10-12 07:51:02 -0700

    Working on training step

--- src/train.py
+++ src/train.py
@@ -1,1 +1,19 @@
-""""""
+"""Train Logistic Regression model with tuned hyperparameters
+"""
+
+import json
+import pickle
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+if __name__ == "__main__":
+    with open('results/tuned_hyperparameters.json') as json_file:
+        hyperparams = json.load(json_file)
+    train = pd.read_csv('data/processed/training.csv')
+    X_train, y_train = train.drop(['condition'], axis=1), train['condition']
+    lr = Pipeline([
+        ('scale', StandardScaler()),
+        ('lr', LogisticRegression(**hyperparams))
+    ])
