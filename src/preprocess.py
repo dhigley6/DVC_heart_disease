b9834c1c0a8d7dfb0c0d3e2eb572a3218fc34a6d
dhigley6 <dhigley@stanford.edu>
2021-10-11 17:52:45 -0700

    Added preprocessing code

---
+++ src/preprocess.py
@@ -0,0 +1,11 @@
+"""Preprocess raw data for modeling
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    data = pd.read_csv('data/raw/heart_cleveland_upload.csv')
+    train, test = train_test_split(data, test_size=0.3)
+    train.to_csv('data/processed/training.csv')
+    test.to_csv('data/processed/test.csv')
